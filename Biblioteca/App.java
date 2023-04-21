9e77f339b91d7c15aef9ef7f08288c2698c8b397
RafaelTwee <raf.asilva557789@gmail.com>
2023-04-21 10:44:42 -0300

    projeto biblioteca

--- Biblioteca/App.java
+++ Biblioteca/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        Livro l1 = new Livro("Pequeno Principe", "Cleofas", "Infantil", "9780306406157");
+        Livro l1 = new Livro("Pequeno Principe", "Companhia das Letras", "Infantil", "9786589711469");
         Usuario u1 = new Usuario("Rafael Silva", "15805444798");
 
         Emprestimo.realizarEmprestimo(l1, u1);
